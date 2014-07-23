c891804d04318e60737ad8517e821b7b167102cf
DIGININJA <keiichi.lindley@gmail.com>
2014-07-22 21:19:48 -0600

    tomnod

--- tomnod/script.js
+++ tomnod/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   console.log('Initiated!');
-  OpenBadges.issue(['http://digininjadev.github.io/tomnod/1.json'], function(errors, successes) { 
+  OpenBadges.issue(['http://afternoon-bayou-5556.herokuapp.com/tomnod/1.json'], function(errors, successes) { 
   	console.log(errors);
   	console.log(successes);
   });
